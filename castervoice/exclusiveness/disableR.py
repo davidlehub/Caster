6961df44a35ed0882fb43d385dad5ffd047b5450
DavidLe <david.le360@gmail.com>
2020-03-19 18:48:37 -0400

    (done: Optimizing by not disable rule not loaded)

--- castervoice/exclusiveness/disableR.py
+++ castervoice/exclusiveness/disableR.py
@@ -10,9 +10,8 @@ def disableR(RulesToDisable_className,aExceptR=[]):
 			# gl.RbeenExclusive.add(RuleToDisable_className)
 			continue
 
-		#--- Optimization do it only if: the rule is loaded	
+		#region--- Optimizing by not disable rule not loaded
 		# print "\n", "ici 20200319091514|  RuleToDisable_className:", RuleToDisable_className, " || In:",stack()[0][3],"%s|%d " % (getframeinfo(currentframe()).filename, getframeinfo(currentframe()).lineno),"| Caller:",stack()[1][3],"%s:%d" % (getframeinfo(stack()[1][0]).filename, getframeinfo(stack()[1][0]).lineno)
-		# print "\t", "ici 20200319091515|  [i.get_rule_class_name() for i in gl.all_loadedRule_mappingRule]:", [i.get_rule_class_name() for i in gl.all_loadedRule_mappingRule]
 
 		#--- (Concatonate/add 2 list without duplicate element)
 		all_loadedRule_class_Name =  list(OrderedDict.fromkeys([i.get_rule_class_name() for i in gl.all_loadedRule_mergeRule] + [i.get_rule_class_name() for i in gl.all_loadedRule_mappingRule]))
@@ -20,26 +19,14 @@ def disableR(RulesToDisable_className,aExceptR=[]):
 		# print "\t", "ici 20200319091515|  [i.get_rule_class_name() for i in gl.all_loadedRule_mappingRule]:", [i.get_rule_class_name() for i in gl.all_loadedRule_mappingRule]
 		# print "\t", "ici 20200319091516|  [i.get_rule_class_name() for i in gl.all_loadedRule_mergeRule]:", [i.get_rule_class_name() for i in gl.all_loadedRule_mergeRule]
 
-
 		if RuleToDisable_className not in all_loadedRule_class_Name:
 			print "\n", "ici20200319091146| skeep to disable rule:", RuleToDisable_className, " || In:",stack()[0][3],"%s|%d " % (getframeinfo(currentframe()).filename, getframeinfo(currentframe()).lineno),"| Caller:",stack()[1][3],"%s:%d" % (getframeinfo(stack()[1][0]).filename, getframeinfo(stack()[1][0]).lineno)
-			# print "\t", "ici 20200319091515|  [i.get_rule_class_name() for i in gl.all_loadedRule_mappingRule]:", [i.get_rule_class_name() for i in gl.all_loadedRule_mappingRule]
 			continue
-		# if RuleToDisable_className not in [i.get_rule_class_name() for i in gl.all_loadedRule_mergeRule]:
-		# 	print "\n", "ici20200319091147| skeep to disable rule :", RuleToDisable_className, " || In:",stack()[0][3],"%s|%d " % (getframeinfo(currentframe()).filename, getframeinfo(currentframe()).lineno),"| Caller:",stack()[1][3],"%s:%d" % (getframeinfo(stack()[1][0]).filename, getframeinfo(stack()[1][0]).lineno)
-		# 	# print "\t", "ici 20200319091516|  [i.get_rule_class_name() for i in gl.all_loadedRule_mergeRule]:", [i.get_rule_class_name() for i in gl.all_loadedRule_mergeRule]
+		#endregion Optimizing by not disable rule not loaded
 
-
-			continue
-
-		#--- 	
-		# try:
+		#--- Do It.	
 		_NEXUS._grammar_manager._change_rule_enabled(RuleToDisable_className, False)
 		try:
 			gl.RbeenExclusive.remove(RuleToDisable_className)
 		except :
 			pass
-		# except:
-		# 	winsound.PlaySound("C:\\- To Burn\\- Project My\\Sound My\\Alarm09.wav", winsound.SND_ASYNC | winsound.SND_ALIAS )
-		# 	print "\n", "20200112213924| got exception with '_NEXUS._grammar_manager._change_rule_enabled'| RuleToDisable_className: ", RuleToDisable_className, " || In:",stack()[0][3],"%s|%d " % (getframeinfo(currentframe()).filename, getframeinfo(currentframe()).lineno),"| Caller:",stack()[1][3],"%s:%d" % (getframeinfo(stack()[1][0]).filename, getframeinfo(stack()[1][0]).lineno)
-		# 	raise ValueError("(see Print above)")
