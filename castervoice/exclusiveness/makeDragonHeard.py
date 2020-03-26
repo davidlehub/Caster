1965ef3242772a3ef2649fddc7d97a3ee9ae4d4a
DavidLe <david.le360@gmail.com>
2020-03-26 08:23:20 -0400

    update makeDragonHeard()

--- castervoice/exclusiveness/makeDragonHeard.py
+++ castervoice/exclusiveness/makeDragonHeard.py
@@ -3,6 +3,7 @@ from typing import Callable, Iterator, Union, Optional, List, Dict
 from castervoice.exclusiveness.DragonModeOnly import DragonModeOnly
 from dragonfly import Playback 
 from castervoice.exclusiveness.Finished_DragonModeOnly import Finished_DragonModeOnly
+from castervoice.exclusiveness.ExclusivMode_class import ExclusivMode
 
 
 #region--- (This is how you annotate a function definitio)
@@ -22,7 +23,9 @@ def makeDragonHeard(pWords, pTime=0.0):
 	'''
 	# print "\n|>--00000000000000| in makeDragonHeard" , " |=> In:",stack()[0][3],"%s|%d " % (getframeinfo(currentframe()).filename, getframeinfo(currentframe()).lineno),"| Caller:",stack()[1][3],"%s:%d" % (getframeinfo(stack()[1][0]).filename, getframeinfo(stack()[1][0]).lineno)
 
-	StateBfore_DragonModeOnly = DragonModeOnly()
+	if  ExclusivMode.enabled: 
+		StateBfore_DragonModeOnly = DragonModeOnly()
+		
 	try:
 		print "\nMake the speech engine heard: ",pWords , " |=> In:",stack()[0][3],"%s|%d " % (getframeinfo(currentframe()).filename, getframeinfo(currentframe()).lineno),"| Caller:",stack()[1][3],"%s:%d" % (getframeinfo(stack()[1][0]).filename, getframeinfo(stack()[1][0]).lineno)
 		Playback([(pWords, pTime)]).execute()
@@ -33,4 +36,5 @@ def makeDragonHeard(pWords, pTime=0.0):
 		raise ValueError(tx)
 	# Playback([(["go", "to", "sleep"], 0.0)]).execute()
 
-	Finished_DragonModeOnly(StateBfore_DragonModeOnly)
+	if  ExclusivMode.enabled: 
+		Finished_DragonModeOnly(StateBfore_DragonModeOnly)
